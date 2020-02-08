ba6cd96e2ec0f6936e59880f418e8426959ca181
Sergej Herbert <herbert.sergej@gmail.com>
2020-02-08 09:12:29 +0100

    Semi deprecate spark

--- spark - wip/images/master/start-master.sh
+++ spark - wip/images/master/start-master.sh
@@ -14,7 +14,7 @@ ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
 cd /spark/bin
 
-/spark/sbin/../bin/spark-class \
+/spark/bin/spark-class \
     org.apache.spark.deploy.master.Master \
     --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT \
     --webui-port $SPARK_MASTER_WEBUI_PORT \
