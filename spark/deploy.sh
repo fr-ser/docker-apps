f5e6e9f041e1855b03f495a827aa84a94334b4e0
Sergej Herbert <herbert.sergej@gmail.com>
2020-02-08 08:30:47 +0100

    User official jupyter pyspark image

--- spark/deploy.sh
+++ spark/deploy.sh
@@ -8,25 +8,22 @@ IMAGE_TAG=":2.4.4"
 start_path=$(pwd)
 
 cd images/base
-DOCKER_IMAGE="${REPO_URI}spark-base${IMAGE_TAG}"
-docker build -t ${DOCKER_IMAGE} -t spark-base .
-docker push ${DOCKER_IMAGE}
+DOCKER_IMAGE_VERSIONED="${REPO_URI}spark-base${IMAGE_TAG}"
+DOCKER_IMAGE_LATEST="${REPO_URI}spark-base:latest"
+docker build -t ${DOCKER_IMAGE_VERSIONED} -t ${DOCKER_IMAGE_LATEST} -t spark-base .
+docker push ${DOCKER_IMAGE_VERSIONED}
 cd ${start_path}
 
 cd images/master
-DOCKER_IMAGE="${REPO_URI}spark-master${IMAGE_TAG}"
-docker build -t ${DOCKER_IMAGE} .
-docker push ${DOCKER_IMAGE}
+DOCKER_IMAGE_VERSIONED="${REPO_URI}spark-master${IMAGE_TAG}"
+DOCKER_IMAGE_LATEST="${REPO_URI}spark-master:latest"
+docker build -t ${DOCKER_IMAGE_VERSIONED} -t ${DOCKER_IMAGE_LATEST} .
+docker push ${DOCKER_IMAGE_VERSIONED}
 cd ${start_path}
 
 cd images/worker
-DOCKER_IMAGE="${REPO_URI}spark-worker${IMAGE_TAG}"
-docker build -t ${DOCKER_IMAGE} .
-docker push ${DOCKER_IMAGE}
-cd ${start_path}
-
-cd images/jupyter
-DOCKER_IMAGE="${REPO_URI}spark-jupyter${IMAGE_TAG}"
-docker build -t ${DOCKER_IMAGE} .
-docker push ${DOCKER_IMAGE}
+DOCKER_IMAGE_VERSIONED="${REPO_URI}spark-worker${IMAGE_TAG}"
+DOCKER_IMAGE_LATEST="${REPO_URI}spark-worker:latest"
+docker build -t ${DOCKER_IMAGE_VERSIONED} -t ${DOCKER_IMAGE_LATEST} .
+docker push ${DOCKER_IMAGE_VERSIONED}
 cd ${start_path}
